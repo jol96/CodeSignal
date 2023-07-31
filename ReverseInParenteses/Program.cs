a91d9b218dc5aae550e5fb172da247a8d3ca5cfe
jol96 <jack1996oloughlin@gmail.com>
2023-07-31 17:15:52 +0100

    reverse strings using stack

---
+++ ReverseInParenteses/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static string ReverseInParentheses(string inputString)
+    {
+        StringBuilder result = new StringBuilder();
+        Stack<StringBuilder> stack = new Stack<StringBuilder>();
+
+        foreach (char c in inputString)
+        {
+            if (c == '(')
+            {
+                // Push the current result to the stack and reset the result for the new content inside parentheses
+                stack.Push(result);
+                result = new StringBuilder();
+            }
+            else if (c == ')')
+            {
+                // Pop the previous result from the stack, reverse the current result, and append it
+                StringBuilder prevResult = stack.Pop();
+                char[] reversedChars = result.ToString().ToCharArray();
+                Array.Reverse(reversedChars);
+                prevResult.Append(reversedChars);
+                result = prevResult;
+            }
+            else
+            {
+                // Append the character to the current result
+                result.Append(c);
+            }
+        }
+
+        return result.ToString();
+    }
+
+
+
+    public static void Main(string[] args)
+    {
+        //Console.WriteLine(ReverseInParentheses("foo(bar)baz"));
+        Console.WriteLine(ReverseInParentheses("foo(bar)baz(blim)"));
+    }
+}
---
+++ SortByHeight/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static int[] SortByHeight(int[] a)
+    {
+        int[] sortedNonNegatives = a.Where(num => num != -1).OrderBy(num => num).ToArray();
+
+        int currentIndex = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] != -1)
+            {
+                a[i] = sortedNonNegatives[currentIndex];
+                currentIndex++;
+            }
+        }
+
+        Console.WriteLine("Sorted Array:");
+        foreach (int num in a)
+        {
+            Console.Write(num + " ");
+        }
+
+        return a;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] a = { -1, 150, 190, 170, -1, -1, 160, 180 };
+
+        // Print the sorted array
+        SortByHeight(a);
+    }
+}
