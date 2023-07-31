a61cf925033c86e3199dc05f601b64aa98d64773
jol96 <jack1996oloughlin@gmail.com>
2023-07-31 12:36:06 +0100

    program to sum first half and second half of an integer and check equality

---
+++ IsLuckyTicket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+    public static bool isLucky(int n)
+    {
+        int ticketLength = Math.Abs(n).ToString().Length;
+
+        int firstHalfSum = 0;
+        int secondHalfSum = 0;
+
+        // Convert the integer to a string
+        string stringValue = n.ToString();
+
+        char[] nCharArray = stringValue.ToCharArray();
+
+        for (int i = 0; i < ticketLength; i++)
+        {
+            // first half sum
+            if (i <= (ticketLength/2) - 1)
+            {
+                firstHalfSum += nCharArray[i];
+            }
+            // second half sum
+            else
+            {
+                secondHalfSum += nCharArray[i];
+            }
+        }
+
+        return firstHalfSum == secondHalfSum;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(isLucky(1212));
+    }
+}
