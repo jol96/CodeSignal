a89fd6877f344725620c804d98af262263ca50e9
jol96 <jack1996oloughlin@gmail.com>
2023-07-31 12:06:13 +0100

    common character count

---
+++ AllLongestStrings/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Program
+{
+    public static string[] AllLongestStrings(string[] inputArray)
+    {
+        // input array of strings
+        // loop through the string array and check the length of each string
+        // have a variable called lengestStringLength set to zero. 
+        // check if each string is longer or equal to longestStringLength
+        // if true add the string to array of strings to be returned
+
+        int longestStringLength = 0;
+        List<string> longestStringsList = new List<string>();
+
+        for(int i = 0; i < inputArray.Length; i++)
+        {
+            if (longestStringLength < inputArray[i].Length)
+            {
+                // set new longestStringLength
+                longestStringLength = inputArray[i].Length;
+
+                // remove all the old strings from array list if a new longest is found
+                if(longestStringsList.Count != 0)
+                {
+                    for(int j = 1; j <= i; j++)
+                    {
+                        longestStringsList.Remove(inputArray[j - 1]);
+                    }  
+                }
+                // add the new longest   
+                longestStringsList.Add(inputArray[i]);
+            }
+            else if(longestStringLength == inputArray[i].Length)
+            {
+                longestStringsList.Add(inputArray[i]);
+            }
+        }
+
+        return longestStringsList.ToArray();
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] strings = { "aba", "aa", "ad", "vcd", "aba" };
+        string[] strings2 = { "abc","eeee","abcd","dcd" };
+
+        Console.WriteLine(String.Join("\n", AllLongestStrings(strings2)));
+    }
+}
---
+++ CommonCharacterCount/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    public static int CommonCharacterCount(string st1, string st2)
+    {
+        // Create dictionaries to count character occurrences
+        Dictionary<char, int> charCount1 = new Dictionary<char, int>();
+        Dictionary<char, int> charCount2 = new Dictionary<char, int>();
+
+        // Count character occurrences in the first string (s1)
+        foreach (char c in st1)
+        {
+            if (charCount1.ContainsKey(c))
+                charCount1[c]++;
+            else
+                charCount1[c] = 1;
+        }
+
+        // Count character occurrences in the second string (s2)
+        foreach (char c in st2)
+        {
+            if (charCount2.ContainsKey(c))
+                charCount2[c]++;
+            else
+                charCount2[c] = 1;
+        }
+
+        // Calculate the number of common characters
+        int commonCount = 0;
+        foreach (var kvp in charCount1)
+        {
+            if (charCount2.TryGetValue(kvp.Key, out int countInS2))
+            {
+                commonCount += Math.Min(kvp.Value, countInS2);
+            }
+        }
+
+        return commonCount;
+    } 
+
+    public static void Main(string[] args)
+    {
+        string s1 = "aabcc";
+        string s2 = "adcaa";
+        int result = CommonCharacterCount(s1, s2);
+        Console.WriteLine(result); // Output: 3
+    }
+}
