1a9d2771420c6c9dafa6eeb6bfcf18bf25d00abb
jol96 <jack1996oloughlin@gmail.com>
2023-08-16 15:57:03 +0100

    challenge 22 - Min jumps to avoid obstractle

---
+++ CoordinateObstacles/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static int MinJumpsToAvoidObstacles(int[] inputArray)
+    {
+        // sort array in ascending order
+        // check first element value
+        // check if first element value+1 exists in the array
+        // if not, set minjump to first element value + 1
+        // if it does, continue to the next element and repeat
+
+        int n = 2;
+        while (inputArray.Any(_ => _ % n < 1))
+        {
+            n++;
+        }
+        return n;
+    }
+
+    public static void Main()
+    {
+        int[] inputArray = { 5, 3, 6, 7, 9 };
+        MinJumpsToAvoidObstacles(inputArray);
+    }
+}
--- IsIPv4Address/Program.cs
+++ IsIPv4Address/Program.cs
@@ -1,13 +1,22 @@
-﻿public class Program
+﻿using System.Text.RegularExpressions;
+
+public class Program
 {
     public static bool IsIPv4Address(string inputString)
     {
+        string pattern = @"\b(0\d|\d0)\d*\b";
+        Regex regex = new(pattern);
         List<bool> checkedValues = new List<bool>(); 
         string[] inputStringSpilt = inputString.Split(new char[] { '.' });
 
+        if(inputStringSpilt.Length != 4)
+        {
+            return false;
+        }
+
         foreach(string value in inputStringSpilt)
         {
-            if(int.TryParse(value, out int intValue))
+            if(int.TryParse(value, out int intValue) && !regex.IsMatch(value))
             {
                 bool isInIPv4Range = intValue >= 0 && intValue <= 255;
                 checkedValues.Add(isInIPv4Range);
