01fbdd3f2430952f92b4265b5cefe10bf13fca10
jol96 <jack1996oloughlin@gmail.com>
2023-08-16 09:35:39 +0100

    Max absolute array difference

---
+++ AreEquallyStrong/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static bool EquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) 
+    {
+        int person1StrongArm = (yourLeft > yourRight) ? yourLeft : yourRight;
+        int person2StrongArm = (friendsLeft > friendsRight) ? friendsLeft : friendsRight;
+
+        int person1WeakArm = (yourLeft < yourRight) ? yourLeft : yourRight;
+        int person2WeakArm = (friendsLeft < friendsRight) ? friendsLeft : friendsRight;
+
+        return person1StrongArm == person2StrongArm && person1WeakArm == person2WeakArm;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(EquallyStrong(10,11,12,10));
+    }
+}
--- PalindroneRearrange/Program.cs
+++ PalindroneRearrange/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    // count character frequency
+    // if the length of the string is even then each character should appear an even amount of times
+    // if the length is odd then at most one character can appear an odd amount of times, the rest should be even.
+
+
+    public static bool CanFormPalindrone(string inputString)
+    {
+        Dictionary<char, int> charFreq = new Dictionary<char, int>();
+        foreach(char c in inputString)
+        {
+            if (charFreq.ContainsKey(c))
+                charFreq[c]++;  
+            else
+                charFreq[c] = 1;      
+        }
+
+        // count number of characters with odd count
+        int oddCount = 0;
+        foreach (int freq in charFreq.Values)
+        {
+            if(freq % 2 != 0)
+                oddCount++;
+        }
+
+        // check palindrone props
+        if (inputString.Length % 2 == 0 && oddCount == 0)
+            return true;
+        if (inputString.Length % 2 != 0 && oddCount == 1)
+            return true;
+
+        return false;
+    }
+}
